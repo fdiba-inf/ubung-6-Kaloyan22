62daaf6cd358d7d66a43743b6ce7f29a984f5e0e
Kaloyan22 <91698321+Kaloyan22@users.noreply.github.com>
2021-11-19 08:06:08 +0000

    .

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,24 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        int num = input.nextInt();
+        while(n<10){
+          System.out.println("Enter a number bigger than 9.");
+          num = input.nextInt();
+        }
+        String pali = Integer.toString(num);
+        int[] arr = new int(pali.length());
+        for(int i=0; i< pali.length(); i++){
+          arr[i] = pali.charAt(i) - '0';
+        }
+        boolean flag = true;
+        for(int i=0; i< pali.length(); i++){
+          if(arr[i]!=arr[pali.length()-i-1]){
+            flag = false;
+          }
+        }
+        System.out.println("Palindrome: " + flag);
+        
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,31 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        boolean val = true;
+        String pass = input.nextLine();
+        do{
+          if(pass.length()<8){
+            val = false;
+          }
+          for(int i=0;i<pass.length();i++){
+              if(!Character.isLetterOrDigit(pass.charAt(i))){
+                val = false;
+              }
+          }
+          int count = 0;
+          for(int i=0;i<pass.length();i++){
+            if(Character.isDigit(pass.charAt(i))){
+              count++;
+            }
+          }
+          if(count<2){
+            val = false;
+          }
+        }while(val==false);
+      if(val=true){
+        System.out.println("Password valid!");
+      }
+
     }
 }
